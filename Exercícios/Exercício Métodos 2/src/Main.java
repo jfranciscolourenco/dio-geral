584f9cd748d79118eb73540b96dec2013820ef96
jfranciscolourenco <jfranciscolourenco@gmail.com>
2022-05-24 15:40:45 -0300

    Adicionando segundo exercício de métodos em Java da DIO.

---
+++ Exercícios/Exercício Métodos 2/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// Quadrilátero
+						
+		System.out.println("Exercício quadrilátero");
+		Quadrilatero.area(3);
+		Quadrilatero.area(5d, 5d);
+		Quadrilatero.area(5, 7, 4);
+		Quadrilatero.area(5f, 6f);
+
+	}
+
+}
